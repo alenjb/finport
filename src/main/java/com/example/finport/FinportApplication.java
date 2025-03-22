993a4ab977cd428011ba531e2dfa4a5c9b27c9b9
alenjb <alenjb@naver.com>
2025-03-22 13:18:16 +0900

    initial commit

---
+++ src/main/java/com/example/finport/FinportApplication.java
@@ -0,0 +1,13 @@
+package com.example.finport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinportApplication.class, args);
+	}
+
+}
